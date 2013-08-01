5f7eafd2e47adca73b7eb7b31ba41dcf7e879d65
piatra <andrei.br92@gmail.com>
2013-08-01 11:56:29 +0200

    updated Gruntfile & package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,21 @@
-/**
-*
-* Copyright 2012 Adobe Systems Inc.;
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-*/
-
 /*global module:false*/
-
 module.exports = function(grunt) {
+
 
     // Project configuration.
     grunt.initConfig({
 
         clean: {
-            release: ['css'],
+            release: ['css']
         },
 
-        stylus: {
-            compile: {
+            stylus: {
+        compile: {
                 options: {
-                    paths: ['node_modules/topcoat-button-base/src/mixins', 'node_modules/topcoat-utils/src/mixins', 'node_modules/topcoat-theme/src'],
-                    import: ['button-mixin', 'utils', 'theme-topcoat-mobile-light'],
+                    paths: ['node_modules/topcoat-utils/src/mixins', 'node_modules/topcoat-button-base/src/mixins', 'node_modules/topcoat-theme/src', 'node_modules/topcoat-theme/src/includes'],
+                    import: ['button-mixin', 'utils', 'theme-topcoat-mobile-light', 'global', 'fonts'],
                     compress: false
                 },
-
                 files: [{
                     src: 'src/topcoat-button.styl',
                     dest: 'css/topcoat-button.css'
@@ -48,10 +29,18 @@ module.exports = function(grunt) {
                 cwd: 'css',
                 src: ['*.css', '!*.min.css'],
                 dest: 'css',
-                ext: '.min.css',
-                options: {
-                    banner: grunt.file.read('src/copyright.styl').toString()
-                }
+                ext: '.min.css'
+            }
+        },
+
+        copy: {
+            release: {
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    src: 'node_modules/topcoat-theme/font/**/*',
+                    dest: 'font'
+                }]
             }
         },
 
@@ -66,10 +55,6 @@ module.exports = function(grunt) {
         },
 
         simplemocha: {
-            options: {
-                ui: 'bdd',
-                reporter: 'Nyan'
-            },
             all: {
                 src: ['test/*.test.js']
             }
@@ -81,6 +66,7 @@ module.exports = function(grunt) {
         }
     });
 
+
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-stylus');
@@ -88,12 +74,12 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-simple-mocha');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task.
     grunt.registerTask('default', ['clean', 'build', 'test', 'release']);
     grunt.registerTask('build', ['stylus', 'jade']);
     grunt.registerTask('test', ['simplemocha']);
-    grunt.registerTask('release', ['cssmin']);
+    grunt.registerTask('release', ['cssmin', 'copy']);
 
 };
-
