48304ce75394efd79f932518818f7800c43ead76
kristoferjoseph <kristoferjoseph@gmail.com>
2013-08-08 16:01:51 -0700

    Adding Topdoc output.

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,13 +22,14 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
         clean: {
             release: ['css'],
         },
 
         stylus: {
-            compile: {
+            mobilelight: {
                 options: {
                     paths: ['node_modules/topcoat-button-base/src/mixins', 'node_modules/topcoat-utils/src/mixins', 'node_modules/topcoat-theme/src'],
                     import: ['button-mixin', 'utils', 'theme-topcoat-mobile-light'],
@@ -37,8 +38,58 @@ module.exports = function(grunt) {
 
                 files: [{
                     src: 'src/topcoat-button.styl',
-                    dest: 'css/topcoat-button.css'
+                    dest: 'css/topcoat-button-mobile-light.css'
                 }]
+            },
+
+            mobiledark: {
+                options: {
+                    paths: ['node_modules/topcoat-button-base/src/mixins', 'node_modules/topcoat-utils/src/mixins', 'node_modules/topcoat-theme/src'],
+                    import: ['button-mixin', 'utils', 'theme-topcoat-mobile-dark'],
+                    compress: false
+                },
+
+                files: [{
+                    src: 'src/topcoat-button.styl',
+                    dest: 'css/topcoat-button-mobile-dark.css'
+                }]
+            },
+
+            desktoplight: {
+                options: {
+                    paths: ['node_modules/topcoat-button-base/src/mixins', 'node_modules/topcoat-utils/src/mixins', 'node_modules/topcoat-theme/src'],
+                    import: ['button-mixin', 'utils', 'theme-topcoat-desktop-light'],
+                    compress: false
+                },
+
+                files: [{
+                    src: 'src/topcoat-button.styl',
+                    dest: 'css/topcoat-button-desktop-light.css'
+                }]
+            },
+
+            desktopdark: {
+                options: {
+                    paths: ['node_modules/topcoat-button-base/src/mixins', 'node_modules/topcoat-utils/src/mixins', 'node_modules/topcoat-theme/src'],
+                    import: ['button-mixin', 'utils', 'theme-topcoat-desktop-dark'],
+                    compress: false
+                },
+
+                files: [{
+                    src: 'src/topcoat-button.styl',
+                    dest: 'css/topcoat-button-dekstop-dark.css'
+                }]
+            }
+        },
+
+        topdoc: {
+            usageguides: {
+                options: {
+                    source: 'css',
+                    destination: './',
+                    template: 'https://github.com/topcoat/usage-guide-theme',
+                    templateData: '<%= pkg.topdoc.templateData %>'
+                }
             }
         },
 
@@ -52,6 +103,17 @@ module.exports = function(grunt) {
                 options: {
                     banner: grunt.file.read('src/copyright.styl').toString()
                 }
+            }
+        },
+
+        copy: {
+            release: {
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    src: 'node_modules/topcoat-theme/img/hamburger_dark.svg',
+                    dest: 'img'
+                }]
             }
         },
 
@@ -88,12 +150,14 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-simple-mocha');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-topdoc');
 
     // Default task.
-    grunt.registerTask('default', ['clean', 'build', 'test', 'release']);
+    grunt.registerTask('default', ['clean', 'build', 'release']);
     grunt.registerTask('build', ['stylus', 'jade']);
     grunt.registerTask('test', ['simplemocha']);
-    grunt.registerTask('release', ['cssmin']);
+    grunt.registerTask('release', ['cssmin', 'copy', 'topdoc']);
 
 };
 
